47ebce5a3a4706b5894589d29acea8e0145b5bae
MrSpock1994 <willpokerpro2016@gmail.com>
2022-09-16 20:08:15 -0300

    About Perl Modules

---
+++ Modules/Communication/Speak.pm
@@ -0,0 +1,11 @@
+package Communication::Speak;
+
+sub test {
+    print"Hello there.\n";
+}
+
+sub greet {
+    print "How are you?\n";
+}
+
+1;
---
+++ Modules/Learning_modules.pm
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+use Communication::Speak;
+use Data::Dumper;
+
+
+sub main {
+    Communication::Speak::test();
+    Communication::Speak::greet();
+}   
+
+
+main();
